67788fd394ae389c675a235bc80238a4b4c01bb8
jhonnyred <73783043+jhonnyred@users.noreply.github.com>
2024-04-24 16:45:47 -0300

    Add files via upload

--- algoritimos/aula8/aula8_AC2/Ac2.java
+++ algoritimos/aula8/aula8_AC2/Ac2.java
@@ -16,43 +16,74 @@ public class Ac2 {
         System.out.print("Insira nota minina global: ");
         float minima = input.nextFloat();
 
-        //NOTAS
+        //NUMERO DE NOTAS
         System.out.print("Digite quantas notas serão calculadas: ");
         int quantia = input.nextInt();
 
-        float valor = 0;
+        boolean check = false;
+        float peso = 0;
+        float peso2 = 0;
         String nota = "";
-        String notas = "";
-        //Aqui foram necessários criar 3 variáveis para manter o sistema com os implementos anteriores
-        //[valor] é a variável onde ocorre as operações e tem seu valor somado a variavel [media], que se tinha antes
-        //A primeira string [nota], no singular, é para conseguir isolar a nota especifica q esta sendo trabalhada,
-        //não seria possível permanecer com a nota mínima somente com uma unica string, pois
-        //notas acima da minima iriam ser armazenadas também
-        //A string [notas] serve para armazenar todas as notas por nome e mostrar isso ao usuário posteriormente
+        String nota2 = "\n\n\n";
+        float valor = 0;
+        //aqui foi necessário criar 2 variáveis para as notas e pesos, basicamente a primeira variavél [nota, peso]
+        //são para executar as operações em cada uma das tentativas do for, enquanto a segunda variável [nota2, peso2]
+        //funcionam como uma memória secundária onde se acumula todas as informações do for. A memória secundária
+        //de [valor] é a variável [média]. Não consegui diminuir o número de variáveis, 
+        //creio que esse é o maior defeito deste código.
+
 
         for (int i = 1; i <= quantia; i++){
-            System.out.print("Nome da nota: ");
-            nota = input.nextLine();
+            check = false;
+            //reseta a verificacao
 
+            //NOME DA NOTA
+            System.out.print("\nNome da nota: ");
+            input.nextLine();
+            nota = input.nextLine();
+            //tive q colocar essa linha extra para conseguir limpar o buffer e remover possíveis "\n"
+            //que ficaram, ele tava skippando esse primeiro scanner
+
+            //PESO
+            System.out.print("Peso: ");
+            peso = input.nextFloat();
+            peso2 = peso2 + peso;
+
+            //VERIFICACAO DE PESO
+            while (check == false){   
+                if(peso2 > 1){
+                    System.out.println("Somatória dos pesos: " + peso2);
+                    System.out.print("A somatória dos pesos é maior que 100%, por favor insira um valor menor: ");
+                    peso2 = peso2-peso;
+                    peso = input.nextFloat();
+                    peso2 = peso2+peso;
+                
+                }else{
+                    check = true;
+                }
+            }
+
+            //NOTA
             System.out.print("Nota: ");
             valor = input.nextFloat();
             nota = nota +": "+ valor + " ";
-            notas = notas + nota + "\n";
+            nota2 = nota2 + nota + "\n";
 
+            //NOTAS MINIMAS
             if (minima > valor){
-                nota_menor = nota_menor + nota; 
+                nota_menor = nota_menor + nota;
+                prodigio = false; 
             }
 
-            System.out.print("Peso: ");
-            valor = valor*input.nextFloat();
-
+            //MEDIA
+            valor = valor*peso;
             media = media + valor;
         }
 
 
 
         // SAÍDA
-        System.out.println(notas);
+        System.out.println(nota2);
         System.out.format("\nMedia Final: %.2f", media);
         if (prodigio == false) {
             System.out.println("\n" + nota_menor);
@@ -60,6 +91,7 @@ public class Ac2 {
         } else {
             System.out.println("\nTodas suas notas estao acima da nota minima, Parabens!!! :D");
         }
+
 
         // APROVACAO
         if (media <= 2) {
@@ -69,7 +101,6 @@ public class Ac2 {
         } else {
             System.out.println("\nAPROVADO");
         }
-
         input.close();
     }
 }
